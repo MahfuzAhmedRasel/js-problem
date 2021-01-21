dddc6814a18022cfafbc62f99cfacc9802c00635
MahfuzAhmedRasel <mahfuzrasel62@.com>
2021-01-22 02:37:22 +0600

    Kilometer To Meter

--- check.js
+++ check.js
@@ -1,15 +1,16 @@
-// https://github.com/MahfuzAhmedRasel/js-problem
-
-    // kilometerToMeter
 function kilometerToMeter(kilometer){
     var  meter = 1;
 
     if(kilometer < 0){
          console.log("Wrong Input");
+       
     }
     else{
         meter = kilometer*1000;
         return meter;
+    
     }
-}
 
+}
+var rasel = kilometerToMeter(1);
+console.log(rasel);
